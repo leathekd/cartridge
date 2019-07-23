e0870267a6c6106c1f10c699beae1e81e99bec09
David Leatherman <git@david.leatherman.fm>
2019-07-23 10:06:23 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
-(defproject sonian/cartridge "1.0.0"
+(defproject cartridge "1.1.0"
   :description "HTTP response playback for clj-http"
-  :url "https://www.github.com/sonian/cartridge"
+  :url "https://www.github.com/leathekd/cartridge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/core.incubator "0.1.3"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/tools.reader "0.8.2"]
-                 [clj-http "0.7.8"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [org.slf4j/slf4j-nop "1.7.5"]
-                                  [ring/ring-jetty-adapter "1.2.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.4:dev"]}
+  :dependencies [[org.clojure/core.incubator "0.1.4"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [clj-http "3.10.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [org.slf4j/slf4j-nop "1.7.26"]
+                                  [ring/ring-jetty-adapter "1.7.1"]]}}
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
                    :all (constantly true)})
